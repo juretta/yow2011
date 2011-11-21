6e87e9aec39fa47151b9119b67c1c15e2e19de2e
Tony Morris <tmorris@tmorris.net>
2011-11-21 14:06:45 +1000

    scrub answers

--- src/L01/List.hs
+++ src/L01/List.hs
@@ -48,53 +48,52 @@ reduceLeft f (h :| t) = foldLeft f h t
 -- Exercise 1
 -- Relative Difficulty: 1
 headOr :: List a -> a -> a
-headOr Nil a    = a
-headOr (h:|_) _ = h
+headOr = error "todo"
 
 -- Exercise 2
 -- Relative Difficulty: 2
 sum :: List Int -> Int
-sum = foldLeft (+) 0
+sum = error "todo"
 
 -- Exercise 3
 -- Relative Difficulty: 2
 length :: List a -> Int
-length = foldLeft (const . succ) 0
+length = error "todo"
 
 -- Exercise 4
 -- Relative Difficulty: 5
 map :: (a -> b) -> List a -> List b
-map f = foldRight (\a b -> f a :| b) Nil
+map = error "todo"
 
 -- Exercise 5
 -- Relative Difficulty: 5
 filter :: (a -> Bool) -> List a -> List a
-filter f = foldRight (\a -> if f a then (a:|) else id) Nil
+filter = error "todo"
 
 -- Exercise 6
 -- Relative Difficulty: 5
 append :: List a -> List a -> List a
-append = flip (foldRight (:|))
+append = error "todo"
 
 -- Exercise 7
 -- Relative Difficulty: 5
 flatten :: List (List a) -> List a
-flatten = foldRight append Nil
+flatten = error "todo"
 
 -- Exercise 8
 -- Relative Difficulty: 7
 flatMap :: (a -> List b) -> List a -> List b
-flatMap f = flatten . map f
+flatMap = error "todo"
 
 -- Exercise 9
 -- Relative Difficulty: 8
 maximum :: List Int -> Int
-maximum = reduceLeft max
+maximum = error "todo"
 
 -- Exercise 10
 -- Relative Difficulty: 10
 reverse :: List a -> List a
-reverse = foldLeft (flip (:|)) Nil
+reverse = error "todo"
 
 -- END Exercises
 
