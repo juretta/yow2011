3fc9b689f9f1e9eb95a3e60b7fa74b3ae2fbf47e
Tony Morris <tmorris@tmorris.net>
2011-11-26 13:33:43 +1000

    functors and monads

--- src/L.hs
+++ src/L.hs
@@ -3,9 +3,12 @@ module L
   module L01.Optional
 , module L01.Validation
 , module L02.List
+, module L03.Fluffy
+, module L03.Misty
 ) where
 
 import L01.Optional
 import L01.Validation
 import L02.List
-
+import L03.Fluffy
+import L03.Misty
---
+++ src/L03/Fluffy.hs
@@ -0,0 +1,35 @@
+module L03.Fluffy where
+
+import Prelude hiding (sum, length, map, filter, maximum, reverse)
+import L01.Optional
+import L01.Validation
+import L02.List
+
+class Fluffy f where
+  furry :: (a -> b) -> f a -> f b
+
+-- Exercise 1
+-- Relative Difficulty: 1
+instance Fluffy List where
+  furry =
+    error "todo"
+
+-- Exercise 2
+-- Relative Difficulty: 1
+instance Fluffy Optional where
+  furry =
+    error "todo"
+
+
+-- Exercise 3
+-- Relative Difficulty: 2
+instance Fluffy Validation where
+  furry =
+    error "todo"
+
+-----------------------
+-- SUPPORT LIBRARIES --
+-----------------------
+
+instance Fluffy [] where
+  furry = fmap
